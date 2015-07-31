ccbe1683674e887c89235e201f332c582b81e265
loet <loet>
2015-07-31 14:39:59 +0200

    added person service unit tests

---
+++ tests/services/person/services.person.tests.js
@@ -0,0 +1,120 @@
+var should = require('should'),
+    Person = require('../../../app/models/person/person.models'),
+    personService = require('../../../app/services/person/person.services');
+
+
+describe('Person Service Unit Tests:', function () {
+    var timestamp = new Date().getTime(), person, id, savedAt, id2;
+
+    beforeEach(function (done) {
+        person = new Person({email: timestamp + '@swissonline.ch', savedAt: new Date().getTime()});
+        person.save(function (err, saved) {
+            id = saved.id;
+            savedAt = saved.savedAt;
+            done();
+        });
+    });
+
+    describe('create tests', function () {
+        it('should create person', function (done) {
+            personService.create({email: timestamp + 'unique@swissonline.ch'})
+                .then(function (person) {
+                    id2 = person.id;
+                    should.equal(person.email, timestamp + 'unique@swissonline.ch');
+                    should.exist(person.savedAt);
+                    done();
+                }, function (err) {
+                    throw err;
+                });
+        });
+        it('should keep emails unique', function (done) {
+            personService.create({email: timestamp + '@swissonline.ch'})
+                .then(function (person) {
+                    should.not.exist(person);
+                }, function (err) {
+                    should.exist(err);
+                    done();
+                });
+        });
+    });
+
+    describe('update tests', function () {
+        it('should update person', function (done) {
+            personService.update(id, {email: 'updated@swissonline.ch', savedAt: savedAt}).
+                then(function (person) {
+                    should.equal(person.email, 'updated@swissonline.ch');
+                    done();
+                }, function (err) {
+                    throw err;
+                });
+        });
+        it('should check for concurrent modifications', function () {
+            personService.update(id, {email: 'updated2@swissonline.ch', savedAt: 123456789}).
+                then(function (person) {
+                    should.not.exist(person);
+                }, function (err) {
+                    should.exist(err);
+                    done();
+                });
+        });
+        it('should keep emails unique', function (done) {
+            personService.create({email: timestamp + 'unique@swissonline.ch'})
+                .then(function (person) {
+                    id2 = person.id;
+                    personService.update(id, {email: timestamp + 'unique@swissonline.ch', savedAt: savedAt})
+                        .then(function (person) {
+                            should.not.exist(person);
+                        }, function (err) {
+                            should.exist(err);
+                            done();
+                        });
+                }, function (err) {
+                    throw err;
+                });
+        });
+    });
+
+    describe('find by id', function () {
+        it('should find by id', function (done) {
+            personService.findById(id)
+                .then(function (person) {
+                    should.exist(person);
+                    done();
+                }, function (err) {
+                    throw err;
+                });
+        });
+        it('should throw error', function (done) {
+            personService.findById(1)
+                .then(function (person) {
+                    should.not.exist(person);
+                }, function (err) {
+                    should.exist(err);
+                    done();
+                });
+        });
+    });
+
+    describe('remove', function () {
+        it('should throw error', function (done) {
+            personService.remove('11aa1aa11a1a1aa111a1aa11')
+                .then(function (result) {
+                    should.not.exist(result);
+                }, function (err) {
+                    should.exist(err);
+                    done();
+                });
+        });
+    });
+
+    afterEach(function (done) {
+        if (id) {
+            personService.remove(id);
+        }
+        if (id2) {
+            personService.remove(id2);
+            id2 = undefined;
+        }
+        done();
+    });
+});
